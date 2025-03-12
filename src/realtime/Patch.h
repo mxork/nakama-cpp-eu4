52e5c35f68161656c54ca444aa864fd02fc0d442
mxork <>
2025-03-12 10:25:35 -0400

    set Patch http_client defines to cloud instance

--- src/realtime/Patch.h
+++ src/realtime/Patch.h
@@ -1,7 +1,12 @@
-#define PATCH_HOST "localhost"
+// #define PATCH_HOST "localhost"
+// #define PATCH_KEY "default_key"
+// #define PATCH_PORT 7350
+// #define PATCH_SSL false
+
+#define PATCH_HOST "34.152.37.181"
 #define PATCH_KEY "default_key"
-#define PATCH_PORT 7350
-#define PATCH_SSL false
+#define PATCH_PORT 443
+#define PATCH_SSL true
 
 void patcher_on_match_join();
 bool patcher_loaded();
